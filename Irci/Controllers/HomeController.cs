2dd24b4667c13fe9ea784bbfc268868d7e8d2bb3
fathoniadi <fathoni.adi@gmail.com>
2016-12-05 16:42:59 +0700

    Make profilmanagercontroller

--- Irci/Controllers/HomeController.cs
+++ Irci/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace Irci.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            return View("index");
         }
 
         public ActionResult About()
