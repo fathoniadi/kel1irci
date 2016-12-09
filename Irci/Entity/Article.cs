ac828f4e117de1fa3d1298527c6b0532dd1c6c03
cahyasetya <cahya.setyaa@gmail.com>
2016-12-09 06:39:59 -0800

    fix entity

--- Irci/Entity/Article.cs
+++ Irci/Entity/Article.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace Irci.Models
+namespace Irci.Entity
 {
     public class Article
     {
---
+++ Irci/Models/ArticleHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Irci.Entity;
+using Npgsql;
+
+namespace Irci.Models
+{
+    public class ArticleHandler
+    {
+        private NpgsqlConnection dbCon;
+        private NpgsqlCommand dbCmd;
+        private List<Article> articles;
+        public ArticleHandler()
+        {
+            dbCon = new Connection().getConnection();
+            dbCon.Open();
+            dbCmd = new NpgsqlCommand();
+        }
+
+        public void getArticles()
+        {
+
+        }
+    }
+}
