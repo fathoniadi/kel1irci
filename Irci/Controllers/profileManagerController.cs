5489cec26d6ca136ed04d9b22d8a5a3acfb97a56
fathoniadi <fathoni.adi@gmail.com>
2016-12-19 19:35:00 +0700

    Register dan login done

--- Irci/Controllers/profileManagerController.cs
+++ Irci/Controllers/profileManagerController.cs
@@ -13,7 +13,6 @@ namespace Irci.Controllers
         // GET: ProfileManager
         ProfileHandler ph = new ProfileHandler();
         List<Profile> profiles;
-        Profile profile;
         public ActionResult Index()
         {
             profiles = ph.getProfiles();
@@ -25,14 +24,6 @@ namespace Irci.Controllers
         {
             profiles = ph.GetProfiles(keyword);
             return View("Profile", profiles);
-        }
-
-        public ActionResult ViewProfile(int idprofile)
-        {
-            var _profile = idprofile.ToString();
-            profile = ph.GetOneProfile(_profile);
-
-            return View();
         }
     }
 }
---
+++ Irci/Models/AuthHandler.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Npgsql;
+
+namespace Irci.Models
+{
+    public class AuthHandler
+    {
+        private NpgsqlConnection dbCon;
+        private NpgsqlCommand dbCmd;
+        public AuthHandler()
+        {
+            dbCon = new Connection().getConnection();
+            dbCon.Open();
+            dbCmd = new NpgsqlCommand();
+            dbCmd.Connection = dbCon;
+        }
+
+
+        public string login(string email, string password)
+        {
+            dbCmd.Connection = dbCon;
+            var idaccount = "";
+            dbCmd.CommandText = "Select idaccount from new_irci.account where emailaccount = '" + email + "' and passaccount = '" + password + "'";
+            try
+            {
+                var result = dbCmd.ExecuteReader();
+
+                while (result.Read())
+                {
+                    idaccount = result[0].ToString();
+                    break;
+                }
+
+
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine(e);
+            }
+            return idaccount;
+        }
+
+        public string register(string email, string password)
+        {
+            dbCmd.Connection = dbCon;
+            dbCmd.CommandText = "Insert into new_irci.account (emailaccount, passaccount, roleaccount) values('"+email+"','"+password+"',1)";
+            try
+            {
+                //dbCmd.Connection.Open();
+                dbCmd.ExecuteNonQuery();
+                System.Diagnostics.Debug.WriteLine("berhasil input");
+               
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine(e);
+            }
+
+            dbCmd.Connection.Close();
+            dbCmd.Connection.Open();
+            var idaccount = "";
+            dbCmd.CommandText = "Select idaccount from new_irci.account where emailaccount = '"+email+"' and passaccount = '"+password+"'";
+            System.Diagnostics.Debug.WriteLine(dbCmd.CommandText);
+            try
+            {
+                var result = dbCmd.ExecuteReader();
+                
+                while (result.Read())
+                {
+                    idaccount = result[0].ToString();
+                    break;
+                }   
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine(e);
+            }
+            return idaccount;
+
+        }
+    }
+}
